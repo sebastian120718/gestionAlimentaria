ffdb5a167128625ecd1b3bdcae0595bd4451a34b
JulianDavidMG <jdmartinezg2@academia.usbbog.edu.co>
2026-04-23 19:36:52 +0000

    clase temperatura

---
+++ productoTemperatura.java
@@ -0,0 +1,23 @@
+public class productoTemperatura extends Producto{
+    private double temperaturaRecomendada;
+
+    public productoTemperatura(String fechaCaducidad, int numeroLote, String fechaEnvasado,String paisOrigen,double temperaturaRecomendada){
+        super(fechaCaducidad,numeroLote,fechaEnvasado,paisOrigen);
+        this.temperaturaRecomendada = temperaturaRecomendada;
+    }
+
+    public double getTemperaturaRecomendada(){
+        return temperaturaRecomendada;
+    }
+    public void setTemperaturaRecomendada(double temperaturaRecomendada){
+        this.temperaturaRecomendada = temperaturaRecomendada;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Fecha de caducidad: " + getFechaCaducidad());
+        System.out.println("Numero de lote: " + getNumeroLote());
+        System.out.println("Fecha de Envasado: " + getFechaEnvasado());
+        System.out.println("Pais de origen: " + getpaisOrigen());
+        System.out.println("Temperatura Recomendada: " + getTemperaturaRecomendada());
+    }
+}
