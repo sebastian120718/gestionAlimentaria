852a91f75cf38c7d31398aa5bdd2835e8dc37bee
sebastian120718 <jsgarciag2@academia.usbbog.edu.co>
2026-04-23 19:18:03 +0000

    mirar si los get en metodo mostrarinfo se ponen

---
+++ productoFresco.java
@@ -0,0 +1,21 @@
+public class productoFresco extends Producto{
+
+    public productoFresco ( String fechaCaducidad , int numeroLote , String fechaEnvasado , String paisOrigen){
+        super (fechaCaducidad, numeroLote,fechaEnvasado, paisOrigen);
+        this.fechaCaducidad = fechaCaducidad;
+        this.numeroLote = numeroLote;
+        this.fechaEnvasado = fechaEnvasado;
+        this.paisOrigen = paisOrigen;
+
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("=== PRODUCTO FRESCO ===");
+        System.out.println("Fecha caducidad: " + getFechaCaducidad());
+        System.out.println("Número lote: " + getNumeroLote());
+        System.out.println("Fecha envasado: " + getFechaEnvasado());
+        System.out.println("País origen: " + getPaisOrigen());
+    }
+
+}
