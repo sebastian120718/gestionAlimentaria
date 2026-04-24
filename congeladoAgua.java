5e1bce94a6fb974e400dcc29c89805e69ea36c64
sebastian120718 <jsgarciag2@academia.usbbog.edu.co>
2026-04-24 01:47:18 +0000

    avanze de clase congeladoagua estar viendo bien los get y set del metodo mostrarinfo

---
+++ congeladoAgua.java
@@ -0,0 +1,28 @@
+public class congeladoAgua extends productoCongelado{
+    private double salinidadAgua;
+
+    public congeladoAgua(String fechaCaducidad, int numeroLote, String fechaEnvasado, String paisOrigen, double temperaturaRecomendada, double salinidadAgua){
+        super(fechaCaducidad, numeroLote, fechaEnvasado, paisOrigen, temperaturaRecomendada);
+        this.salinidadAgua = salinidadAgua;
+
+    }
+
+    public double getSalinidadAgua(){
+        return salinidadAgua;
+    }
+    public void setSalinidadAgua ( double salinidadAgua){
+        this.salinidadAgua= salinidadAgua;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("=== CONGELADO POR AGUA ===");
+        System.out.println("Fecha caducidad: " + getFechaCaducidad());
+        System.out.println("Número lote: " + getNumeroLote());
+        System.out.println("Fecha envasado: " + getFechaEnvasado());
+        System.out.println("País origen: " + getPaisOrigen());
+        System.out.println("Temperatura recomendada: " + getTemperaturaRecomendada());
+        System.out.println("Salinidad agua: " + getSalinidadAgua());
+    }
+
+}
